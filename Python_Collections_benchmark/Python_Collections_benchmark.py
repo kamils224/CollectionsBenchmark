d85477efa3d35c0bffd2982557494d7b5c063761
kamils224 <kamil.sagalara@student.put.poznan.pl>
2019-11-12 21:32:08 +0100

    Some updates

--- Python_Collections_benchmark/Python_Collections_benchmark.py
+++ Python_Collections_benchmark/Python_Collections_benchmark.py
@@ -1,7 +1,40 @@
+import sys
+
 from Benchmark import Benchmark
 from ListTest import ListTest
 
+def ignore_exception(IgnoreException=Exception,DefaultVal=None):
+    """ Decorator for ignoring exception from a function
+    e.g.   @ignore_exception(DivideByZero)
+    e.g.2. ignore_exception(DivideByZero)(Divide)(2/0)
+    """
+    def dec(function):
+        def _dec(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except IgnoreException:
+                return DefaultVal
+        return _dec
+    return dec
+
+
 if __name__ == '__main__':
+
+    samples = 10
+    num_of_operations = 100
+    success = False
+
+    sint = ignore_exception(ValueError)(int)
+
+    if len(sys.argv) == 4:
+        success = True
+        if sys.argv[0] == '-s':
+            samples = sint(sys.argv[1])
+        if samples is None:
+            samples = 10
+            success = False
+
+
 
     b = Benchmark(ListTest(1000))
     result = b.perform_all_tests()
--- Python_Collections_benchmark/ResultsManager.py
+++ Python_Collections_benchmark/ResultsManager.py
@@ -1,4 +1,27 @@
 
 
 class ResultsManager():
-    pass
+    _collection_info = 'Collection name: {},number of operations: {},unit: {}'
+    _columns = 'Add time,Find time,Remove time'
+    _row_pattern = '{},{},{}'
+    _measure_unit = "ms"
+
+    collection_name = "None"
+    number_of_operations = 0
+
+    def save_to_csv(self,results :list, path:str):
+        csv = list()
+        first_row = self._collection_info.format\
+            (self._collection_info, self.collection_name, self.number_of_operations, self._measure_unit)
+
+        csv.append(first_row)
+        csv.append(self._columns)
+
+        for item in results:
+            row = self._row_pattern.format(str(item.add_time), str(item.find_time), str(item.remove_time))
+            csv.append(row)
+
+        output = '\n'.join(csv)
+
+        with open(path) as file:
+            file.write(output)
